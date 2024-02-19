dc1d809c62c57b94b604fecb642301c11ded8dbc
suhail khan <khansohail015@gmail.com>
2024-02-19 17:05:07 +0530

    update animation

---
+++ Batch-02/19-02-2024/index.js
@@ -0,0 +1,21 @@
+
+console.log("value");///value
+
+function add (a, b) {
+    console.log(a + b);
+}
+add(3, 5);///8
+setTimeout(() => {
+    console.log("i am second");
+}, 3000);
+console.log("data");///data
+
+// function value () {
+//     console.log("i am first");///first
+//     setTimeout(() => {
+//         console.log("i am second");
+//     }, 2000);
+//     console.log("i am third");//third
+// }
+// value()
+
