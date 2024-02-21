fab420472c6cafd76a1c79700c4fcbd7cb65134c
suhail khan <khansohail015@gmail.com>
2024-02-21 17:52:08 +0530

    revision classes constructor function

--- my-tailwind-project/src/App.js
+++ my-tailwind-project/src/App.js
@@ -7,19 +7,22 @@ import { ChakraProvider } from '@chakra-ui/react'
 import Login from './views/login/Login'
 import Navbar2 from './component/navbar/Nav2'
 import GitHubCommitHistory from './component/items/Cal'
+import {Card} from './Card'
 const App = () => {
+
   return (
     <div>
       <Navbar />
+      <Card name="pooja kumari" city="jaipur" />
+      <Card name="sara kumari" city="lucknow" />
+      <Card name="moona kumari" city="karooli"/>
+      <Card name="sunny kumari"  city="usa"/>
 
-      <ChakraProvider>
-        <Item />
-      </ChakraProvider>
-      <MapComponent />
-      <Navbar2 />
+      {/* <Navbar2 /> */}
       {/* <Login /> */}
       {/* <GitHubCommitHistory /> */}
 
+      <h1>hello manish</h1>
     </div>
   )
 }
---
+++ my-tailwind-project/src/Card.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Card = (props) => {
+  return (
+    <div>
+          <h1>Hello,{props.name}----{props.city }</h1>
+    </div>
+  )
+}
+
+// export default Card
