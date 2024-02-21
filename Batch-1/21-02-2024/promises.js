9abe2ff1d9479c38c7982fa10d73726dc7311f02
suhail khan <khansohail015@gmail.com>
2024-02-21 21:40:25 +0530

    revision of promisses

---
+++ Batch-1/21-02-2024/promises.js
@@ -0,0 +1,17 @@
+// fetch("https://jsonplaceholder.typicode.com/users")
+//     .then((Response) => {
+//         return Response.json()
+
+//     }).then((data)=>console.log(data))
+//     .catch((error)=>console.log(error))
+
+// const promise = fetch("https://jsonplaceholder.typicod.com/users");
+
+// console.log("Promise status:", promise);
+
+// promise
+//     .then((response) => {
+//         return response.json();
+//     })
+//     .then((data) => console.log(data))
+//     .catch((error) => console.log("error is",error.message));
