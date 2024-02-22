a6b86e52de8ae9562cabcd21a395210cda90b3ae
suhail khan <khansohail015@gmail.com>
2024-02-22 11:35:48 +0530

    chakra teplate update

--- my-tailwind-project/src/App.js
+++ my-tailwind-project/src/App.js
@@ -8,21 +8,36 @@ import Login from './views/login/Login'
 import Navbar2 from './component/navbar/Nav2'
 import GitHubCommitHistory from './component/items/Cal'
 import {Card} from './Card'
+import JoinOurTeam from './Form'
 const App = () => {
 
   return (
     <div>
       <Navbar />
-      <Card name="pooja kumari" city="jaipur" />
+      {/* <Card name="pooja kumari" city="jaipur" />
       <Card name="sara kumari" city="lucknow" />
       <Card name="moona kumari" city="karooli"/>
       <Card name="sunny kumari"  city="usa"/>
+      <Card name="mohit"  city="usa"/>
+      <Card name="sitaram "  city="usa"/>
+      <Card name="mukesh " city="usa" /> */}
+      {/* <Card name="sara kumari" city="lucknow" />
+      <Card name="moona kumari" city="karooli" />
+      <Card name="sunny kumari" city="usa" />
+      <Card name="mohit" city="usa" />
+      <Card name="sitaram " city="usa" />
+      <Card name="mukesh " city="usa" /> */}
+
 
       {/* <Navbar2 /> */}
       {/* <Login /> */}
       {/* <GitHubCommitHistory /> */}
 
       <h1>hello manish</h1>
+
+      <ChakraProvider>
+        <JoinOurTeam />
+      </ChakraProvider>
     </div>
   )
 }
