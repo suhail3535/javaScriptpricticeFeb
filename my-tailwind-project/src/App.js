c0306c2c7cdc479faa8bfc6aba1b16bbbf8e6e12
suhail khan <khansohail015@gmail.com>
2024-02-25 19:33:38 +0530

    product card completed

--- my-tailwind-project/src/App.js
+++ my-tailwind-project/src/App.js
@@ -1,60 +1,21 @@
-import React, { useState } from 'react'
-import axios from 'axios'
-// import Item from './component/items/Item'
+
+import React from 'react'
 import Navbar from './component/navbar/Navbar'
-import MapComponent from './component/footer/Footer'
-import { ChakraProvider } from '@chakra-ui/react'
-import Login from './views/login/Login'
 import Navbar2 from './component/navbar/Nav2'
-import GitHubCommitHistory from './component/items/Cal'
-import { Card } from './Card'
-import JoinOurTeam from './Form'
+import Login from './views/login/Login'
+import JoinOurTeam from './views/login/Form'
 import Item from './component/items/Item'
-import Temp from './views/Temp'
-// import axios from 'axios'
+import { ChakraProvider } from '@chakra-ui/react'
+
 const App = () => {
-  const [item, setItem] = useState([])
-  const getData = async () => {
-    try {
-      let res = await axios.get("https://jsonplaceholder.typicode.com/todos")
-      console.log(res.data)
-      setItem(res.data)
-    } catch (error) {
-      console.log(error)
-    }
-  }
-
-  getData()
-
   return (
     <div>
       <Navbar />
-      {/* <Card name="pooja kumari" city="jaipur" />
-      <Card name="sara kumari" city="lucknow" />
-      <Card name="moona kumari" city="karooli"/>
-      <Card name="sunny kumari"  city="usa"/>
-      <Card name="mohit"  city="usa"/>
-      <Card name="sitaram "  city="usa"/>
-      <Card name="mukesh " city="usa" /> */}
-      {/* <Card name="sara kumari" city="lucknow" />
-      <Card name="moona kumari" city="karooli" />
-      <Card name="sunny kumari" city="usa" />
-      <Card name="mohit" city="usa" />
-      <Card name="sitaram " city="usa" />
-      <Card name="mukesh " city="usa" /> */}
-
-
       {/* <Navbar2 /> */}
+      {/* <JoinOurTeam /> */}
       {/* <Login /> */}
-      {/* <GitHubCommitHistory /> */}
+      <ChakraProvider>   <Item /></ChakraProvider>
 
-      {/* <h1>hello manish</h1> */}
-
-      {/* <ChakraProvider>
-        <JoinOurTeam />
-      </ChakraProvider> */}
-      {/* <Item /> */}
-      <Temp />
     </div>
   )
 }
--- my-tailwind-project/src/component/items/Item.jsx
+++ my-tailwind-project/src/component/items/Item.jsx
@@ -4,6 +4,7 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import ProductCard from "../reusableCard/ProductCard";
 import { Box, Skeleton, SkeletonCircle, SkeletonText } from "@chakra-ui/react";
+import Example from "../reusableCard/ItemCard";
 const Item = () => {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true);
@@ -12,8 +13,9 @@ const Item = () => {
     try {
       let res = await axios.get(
         "https://helpful-gray-rooster.cyclic.app/product"
+
       );
-      console.log(res,"item")
+      console.log(res.data,"item")
       setData(res.data);
       setLoading(false);
     } catch (error) {
@@ -28,7 +30,7 @@ const Item = () => {
 
   return (
     <>
-      {loading ? (
+      {/* {loading ? (
         <div>
           <Box padding="6" boxShadow="lg" bg="white">
             <SkeletonCircle size="20" />
@@ -36,24 +38,10 @@ const Item = () => {
           </Box>
         </div>
       ) : (
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2">
-          {data.map((item) => {
-            return (
-              <ProductCard
-                key={item.id}
-                id={item.id}
-                image={item.Link}
-                name={item.Name}
-                Rating={item.Brand}
-                size={item.Brokenness}
-                url={item.URL}
-                price={item.MRP}
-                discount={item.Discount}
-              />
-            );
-          })}
+        <div>
         </div>
-      )}
+      )} */}
+      <Example products={data} />
     </>
   );
 };
