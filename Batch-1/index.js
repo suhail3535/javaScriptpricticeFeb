df5e982b78273f281f28e7119c9651102c0d8d59
suhail khan <khansohail015@gmail.com>
2024-03-05 15:25:51 +0530

    javascript revision

---
+++ Batch-1/index.js
@@ -0,0 +1,17 @@
+
+// write a program that return sum of two numbers
+
+let number1 = 10;
+let number2 = "10";
+let number3="mukesh"
+if (number1 === number2) {
+    console.log("manish")
+} else {
+    console.log("manish ka jhoota")
+
+}
+
+// function sum (a,b) {
+// console.log(a-b)
+// }
+// sum(5,2)
