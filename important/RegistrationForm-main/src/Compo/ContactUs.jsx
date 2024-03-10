133d842f5e603d5086902f807f73903bc1db7a3f
suhail khan <khansohail015@gmail.com>
2024-03-10 21:41:08 +0530

    login

--- important/RegistrationForm-main/src/Compo/ContactUs.jsx
+++ important/RegistrationForm-main/src/Compo/ContactUs.jsx
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, { useInsertionEffect, useState } from 'react'
 
 const ConatctUs = () => {
+  const [data, setData] = useState([])
+  // useInsertionEffect
   return (
     <div>
       {/* ConatctUs */}
