1eeb568b655306a763f908ab90ddb8067e031d60
suhail khan <khansohail015@gmail.com>
2024-03-11 11:35:30 +0530

    introductiuon

---
+++ Batch-02/javascriptbatch-2/intro.js
@@ -0,0 +1,25 @@
+let a;
+a = 10
+a = 20
+a=30
+console.log(a)
+document.write(a)
+const b = 50;
+// var=100
+var c = 150
+c = 200
+c=309
+console.log(b);
+console.log(c);
+
+document.writeln(b)
+document.writeln(c)
+
+let data = 1000;
+if (data === 100) {
+     a=170
+} else {
+    console.log("not applicable")
+}
+
+console.log(a,"line 25")
