50702285d1c733254b634ca2980cf7ab451a1285
suhail khan <khansohail015@gmail.com>
2024-03-12 11:41:00 +0530

    practice of variables and data types

---
+++ Batch-02/javascriptbatch-2/datatype.js
@@ -0,0 +1,46 @@
+let persionInfo = {
+    name: "madan lal",
+    age: 29,
+    mobile: 999999999,
+    post: "SP"
+}
+// console.log(persionInfo,"output from line 7")
+// console.log(persionInfo.name, persionInfo.age)
+
+// let data = [1, 2, 3, 4, 5, 6, "chunnu", "munnu"]
+// console.log(data[6]);
+
+let arr = [
+    {
+        name: "madan lal",
+        age: 29,
+        mobile: 999999999,
+        post: "SP"
+    },
+    {
+        name: "chunnu lal",
+        age: 29,
+        mobile: 999999999,
+        post: "SP"
+    },
+    {
+        name: "sohan lal",
+        age: 29,
+        mobile: 999999999,
+        post: "SP"
+    }
+
+]
+// let newdata=arr[0]
+// console.log(newdata.name,newdata.age);
+// console.log(arr[0].name)
+function value () {
+    let persionInfo = {
+        name: "madan lal",
+        age: 29,
+        mobile: 999999999,
+        post: "SP"
+    }
+    console.log(persionInfo)
+}
+value()
