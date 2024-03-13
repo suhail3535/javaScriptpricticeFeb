c0041c15673cc3500c8298f660e1b6f3bfc9a8b5
suhail khan <khansohail015@gmail.com>
2024-03-13 15:23:21 +0530

    logical operator

---
+++ Batch-02/javascriptbatch-2/operator.js
@@ -0,0 +1,37 @@
+// Mathematical operator
+
+// let a = "20";
+// let b = 20
+
+// let c = a !== b;
+// console.log(c);
+
+// let d;
+// let mohit = "chunnu";
+// console.log(d!=mohit);//
+// console.log(d);
+
+
+// <-----------Task for Today-------->
+// question-1
+// explain Mathematical operators in javascrit
+
+// question-2
+// explain Comparison Operators in javascrit
+
+// write a program of add two numbers
+// write a program of Subtraction two numbers
+// write a program of Multiplication two numbers
+// write a program of division two numbers
+// write a program by using % Modulus (remainder) to check remainder of two numbers
+
+
+// let a = 10;
+// let b = 20;
+// let c = 100;
+// let d = 200;
+
+// console.log((a == 10 && b == 20) && (c == 100 && d == 200))
+// console.log(a==10 || b== 30)
+
+
