3d396012b25c7cedd4bc60da699c0a871f73663d
suhail khan <khansohail015@gmail.com>
2024-03-15 17:03:23 +0530

    update  cart and quatity sorting and filter

---
+++ Batch-02/javascriptbatch-2/conditional.js
@@ -0,0 +1,12 @@
+let a = 100;
+let b = 200;
+let c = 50;
+if (a>b) {
+console.log("a is smaller ");
+} else if(a>c){
+    console.log("a is grater");
+
+} else {
+ console.log("output from else statement");
+}
+// question:-check if a>b, if it is true then print "you are right" in console otherwise print "you are wrong"
