40bce5dd37c2f781752f2c64308bd8b951efc2ee
suhail khan <khansohail015@gmail.com>
2024-03-21 14:28:04 +0530

    update code

--- Batch-02/javascriptbatch-2/obj.js
+++ Batch-02/javascriptbatch-2/obj.js
@@ -4,19 +4,32 @@ let ramu = {
     age: 30,
     mobile: 9498898980,
     city: "Delhi",
-    state:{
-        school: "dps",
-        class:10
-    }
+
 
 }
-console.log(ramu["state"]["class"]);//braket notation
-console.log(ramu.state.class);//dot notation
+// console.log(ramu["state"]["class"]);//braket notation
+// console.log(ramu.state.class);//dot notation
 // how to access value from object
 //bracket Notation
-console.log(ramu["fname"],ramu["age"])
-console.log(ramu["mobile"], ramu["city"])
+// console.log(ramu["fname"],ramu["age"])
+// console.log(ramu["mobile"], ramu["city"])
 
 //Dot Notation
-console.log(ramu.age,ramu.city)
-console.log(ramu.name)
+// console.log(ramu.age,ramu.city)
+// console.log(ramu.name)
+
+
+// how to Delete key in excisting obj
+
+let data = {
+    name: 'aaa',
+    age: 27,
+    city:"Delhi"
+}
+console.log(data);
+
+
+delete data.city
+delete data.name
+console.log(data);
+
