4c6c29cadb9779dbad42014bbe2ac0d438158e20
suhail khan <khansohail015@gmail.com>
2024-03-22 12:48:16 +0530

    pegination update

--- Batch-02/javascriptbatch-2/arr.js
+++ Batch-02/javascriptbatch-2/arr.js
@@ -4,21 +4,40 @@ let obj = {
 }
 let kuchbhi = {city:"jaipur rajasthan",state:"rrr"}
 
-let data = ["sunil", 20, "manish", "mukesh", 40, true, false,5,7,9]
+let data = ["sunil", "manish", "mukesh"]
 
+
+// how to add value in array
+// 1-push Method() add value at last index
+data.push("naman")
+data.push({name:"chunnu",age:30})
+// 2-how to add value at starting position index
+data.unshift("mukesh")
+
+// how to delete from an array
+
+data.pop()
+data.shift()
 console.log(data);
 
 
 
 
 
-// let data1 = [
-//     { name: "sunil", age: 30 },
-//     { name: "sumit", age: 30 },
-//     { name: "sonal", age: 50 },
-//     { name: "sunil", age: 40 },
-//     { name: "sunil", age: 40 },
 
 
-// ]
-// console.log(data1);
+
+
+let arr2 = ["ram", "rohit", 3, 6, "vivek"]
+arr2.push("dk")
+let c = arr2.length - 1;
+console.log(c);
+arr2.pop()
+arr2.unshift("shyam")
+
+console.log(arr2)
+
+
+
+
+
---
+++ Batch-1/index.js
@@ -0,0 +1,7 @@
+var password = "vbhhhhh";
+if (password.length < 6) {
+    console.log("Invalid : Your Password must be atleast 6 characters long");
+}
+else {
+    console.log("Valid Password");
+}
