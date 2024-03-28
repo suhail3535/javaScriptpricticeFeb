4ebfaf400cd27ca23390a498307e35852bf36f03
suhail khan <khansohail015@gmail.com>
2024-03-28 10:57:17 +0530

    arrow functon

--- Batch-02/javascriptbatch-2/function.js
+++ Batch-02/javascriptbatch-2/function.js
@@ -1,17 +1,26 @@
 
 // <------Normal Function---------->
-let a = 10;let b=20
-function get () {
-    console.log(a + b)
-    console.log("hello from javascript")
+// let a = 10;let b=20
+// function get () {
+//     console.log(a + b)
+//     console.log("hello from javascript")
 
-}
+// }
 // get()
 
-function calValue (a,b) {
-    console.log("sum of a and b",a+b);
-    console.log(`product of a and b is  ${a*b} difference of  a and b is ${a-b}`);
-    console.log(a-b);
-    console.log(a/b);
+// function calValue (a,b) {
+//     console.log("sum of a and b",a+b);
+//     console.log(`product of a and b is  ${a*b} difference of  a and b is ${a-b}`);
+//     console.log(a-b);
+//     console.log(a/b);
+// }
+// calValue(40, 10)
+
+
+// <------------arrow function-------->
+let data = (a,b)=> {
+    console.log("sum of a and b", a + b);
+    console.log(`product of a and b is  ${a * b} difference of
+    a and b is ${a - b}`);
 }
-calValue(40, 10)
+data(100,20)
