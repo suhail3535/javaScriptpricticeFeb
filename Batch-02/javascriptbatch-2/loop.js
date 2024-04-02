126fbf5f91280848e1571b67b284440c5066f42f
suhail khan <khansohail015@gmail.com>
2024-04-02 14:30:58 +0530

    update while loop

---
+++ Batch-02/javascriptbatch-2/loop.js
@@ -0,0 +1,31 @@
+// Question-1 print 1 to 100
+// output like:-1 2 3 4
+// Question-2 print 1 to 100
+// output like:1
+// output like:2
+// output like:3
+// Question 3 print sum of all number from 1 to 50
+
+
+
+
+
+
+let bag = ""
+let i = 10;//starting point
+while (i>0) {//conditions
+
+    bag=bag+i+"_" //1 2 3
+        i--//increment
+}
+console.log( bag);
+
+
+// let sum = 0;
+// let i = 1;
+// while (i<=5) {
+//     // console.log(i,"i value");
+//     sum = sum + i//
+//     i++
+// }
+// console.log(sum,"sum value");
