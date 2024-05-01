cbe05894fc1f2560cabf94eb75834e371fa99307
suhail khan <khansohail015@gmail.com>
2024-05-01 20:16:47 +0530

    update

--- Batch-02/May/function.js
+++ Batch-02/May/function.js
@@ -5,12 +5,56 @@
 
 
 
-function addNumber (a, b,c) {
-    // return a + b
-    // return a-b
-console.log(a+b+c);
-console.log(a-b-c);
-}
+// function addNumber (a, b,c) {
+//     // return a + b
+//     // return a-b
+// console.log(a+b+c);
+// console.log(a-b-c);
+// }
 // addNumber(10,50,4)
 // console.log(value);
 // console.log(addNumber(2,4))
+
+// print number from 1 to N
+// print even number from 1 to M
+// let i = 1;
+// let N = 100;
+// let M = 16
+// let j=1
+// function printNumber () {
+//     while(i <= N) {
+//         console.log(i);
+//         i++
+//     }
+
+//     while(j <= M) {
+//         if (j % 2 == 0) {
+//             console.log("Even number is",j)
+//         }
+//         j++
+//     }
+
+//     // turnary operator
+//     N < M ? console.log("true") : console.log("false");
+
+// }
+// printNumber()
+// let ctr = a;
+// console.log(ctr);
+
+// Write a function that takes a character as input and determines
+// whether it's a vowel or a consonant. Assume the input is a single letter.let
+
+
+
+
+
+function check(char){
+
+    if (char == "a" || char == "i" || char == "o" || char == "e" || char == "u") {
+        console.log("vowel");
+    } else {
+        console.log("consonent");
+    }
+}
+check("s")
