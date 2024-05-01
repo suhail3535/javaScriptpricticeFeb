e4c9b2b24943f65c62aefe6b1ee6d8bddf7cbb74
suhail khan <khansohail015@gmail.com>
2024-05-01 14:29:28 +0530

    expalin fuction concept to trainee

---
+++ Batch-02/May/function.js
@@ -0,0 +1,16 @@
+// A javascript function is block of code that
+//  design to perform for a specificx task.
+// When we invoke(call) function then the code will executaed.
+
+
+
+
+function addNumber (a, b,c) {
+    // return a + b
+    // return a-b
+console.log(a+b+c);
+console.log(a-b-c);
+}
+// addNumber(10,50,4)
+// console.log(value);
+// console.log(addNumber(2,4))
